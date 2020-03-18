bb92c939d34420de79fdcacc8554bce3eff92962
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-03-18 15:16:02 -0500

    Saving to localStorage functions added

--- movie_seat_booking/script.js
+++ movie_seat_booking/script.js
@@ -6,9 +6,26 @@ const movieSelect = document.getElementById('movie');
 
 let ticketPrice = +movieSelect.value;
 
+// Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
 //updates the ticket count and total
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  // Copy selected seats into array
+  // Map through array
+  // return a new array indexes
+
+  //reminder: spread operator does not pass in the nodelist but the values into selectedSeats
+  // map, similar to forEach (which returns nothing), but map returns an array
+
+  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
 
   const selectedSeatsCount = selectedSeats.length;
 
@@ -21,6 +38,7 @@ function updateSelectedCount() {
 //movie select event
 movieSelect.addEventListener('change', e => {
   ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 });
 
