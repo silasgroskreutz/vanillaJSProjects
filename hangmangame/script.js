2b3a256df7a541c2ace38e8bbcdf5199a4bd2a52
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-04-11 10:15:37 -0500

    JS start. displaying Word function created

---
+++ hangmangame/script.js
@@ -0,0 +1,38 @@
+const wordEl = document.getElementById('word');
+const wrongLettersEl = document.getElementById('wrong-letters');
+const playAgainBtn = document.getElementById('play-again');
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container');
+const finalMessage = document.getElementById('final-message');
+
+const figureParts = document.querySelectorAll('.figure-part');
+
+const words = ['saintbernard', 'greatdane', 'greyhound', 'bordercollie'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+const correctLetters = [''];
+const wrongLetters = [];
+
+function displayWord() {
+  wordEl.innerHTML = `
+        ${selectedWord
+          .split('')
+          .map(
+            (letter) => `
+            <span class="letter">
+                ${correctLetters.includes(letter) ? letter : ''}
+            </span>
+            `
+          )
+          .join('')}
+    `;
+  const innerWord = wordEl.innerText.replace(/\n/g, '');
+
+  if (innerWord === selectedWord) {
+    finalMessage.innerText = 'Congratulations! You Won!';
+    popup.style.display = 'flex';
+  }
+}
+
+displayWord();
