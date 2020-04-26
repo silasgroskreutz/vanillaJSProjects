49e65e8de91c5577cd98c8bbe1d61ee769cb93dd
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-04-26 07:20:10 -0500

    basic post filter created

--- infinite_scroll/script.js
+++ infinite_scroll/script.js
@@ -50,6 +50,23 @@ function showLoading() {
   }, 1000);
 }
 
+// Filter posts by input
+function filterPosts(e) {
+  const term = e.target.value.toUpperCase();
+  const posts = document.querySelectorAll('.post');
+
+  posts.forEach((post) => {
+    const title = post.querySelector('.post-title').innerText.toUpperCase();
+    const body = post.querySelector('.post-body').innerText.toUpperCase();
+
+    if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+      post.style.display = 'flex';
+    } else {
+      post.style.display = 'none';
+    }
+  });
+}
+
 // Show initial posts
 showPosts();
 
@@ -57,6 +74,8 @@ window.addEventListener('scroll', () => {
   const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
 
   if (scrollTop + clientHeight >= scrollHeight - 5) {
-    showLoading;
+    showLoading();
   }
 });
+
+filter.addEventListener('input', filterPosts);
