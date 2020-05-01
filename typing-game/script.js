f9afaa512e769fd7964dfb3d215cbf8ae8b2ba5d
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-04-30 21:14:10 -0500

    JS changes Timer working Endgame set

--- typing-game/script.js
+++ typing-game/script.js
@@ -2,7 +2,7 @@ const word = document.getElementById('word');
 const text = document.getElementById('text');
 const scoreEl = document.getElementById('score');
 const timeEl = document.getElementById('time');
-const endgameEl = document.getElementById('end-game');
+const endgameEl = document.getElementById('end-game-container');
 const settingsBtn = document.getElementById('settings-btn');
 const settings = document.getElementById('settings');
 const settingsForm = document.getElementById('settings-form');
@@ -41,6 +41,12 @@ let score = 0;
 //Init Time
 let time = 10;
 
+//Focus on text on start
+text.focus();
+
+// Start counting down
+const timeInterval = setInterval(updateTime, 1000);
+
 //Generate random word from array
 function getRandomWord() {
   return words[Math.floor(Math.random() * words.length)];
@@ -58,6 +64,29 @@ function updateScore() {
   scoreEl.innerHTML = score;
 }
 
+// Update Time
+function updateTime() {
+  time--;
+  timeEl.innerHTML = time + 's';
+
+  if (time === 0) {
+    clearInterval(timeInterval);
+    // end game
+    gameOver();
+  }
+}
+
+// Game Over, show end screen
+function gameOver() {
+  endgameEl.innerHTML = `
+    <h1>Time ran out</h1>
+    <p>Your final score is ${score}</p>
+    <button onclick="location.reload()">Reload</button>
+  `;
+
+  endgameEl.style.display = 'flex';
+}
+
 addWordToDOM();
 
 // Event Listener
@@ -70,5 +99,9 @@ text.addEventListener('input', (e) => {
 
     //clear
     e.target.value = '';
+
+    time += 5;
+
+    updateTime();
   }
 });
