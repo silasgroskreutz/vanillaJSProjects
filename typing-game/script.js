84e4f6a1f1b3efa4ef71b3d0d9e2a38a1f0588d9
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-05-01 10:52:43 -0500

    Added difficulty changes to site

--- typing-game/script.js
+++ typing-game/script.js
@@ -41,6 +41,18 @@ let score = 0;
 //Init Time
 let time = 10;
 
+// Set difficulty to value in local storage or medium
+let difficulty =
+  localStorage.getItem('difficulty') !== null
+    ? localStorage.getItem('difficulty')
+    : 'medium';
+
+// Set difficulty to set value
+difficultySelect.value =
+  localStorage.getItem('difficulty') !== null
+    ? localStorage.getItem('difficulty')
+    : 'medium';
+
 //Focus on text on start
 text.focus();
 
@@ -91,6 +103,7 @@ addWordToDOM();
 
 // Event Listener
 
+//Typing
 text.addEventListener('input', (e) => {
   const insertedText = e.target.value;
   if (insertedText === randomWord) {
@@ -100,8 +113,23 @@ text.addEventListener('input', (e) => {
     //clear
     e.target.value = '';
 
-    time += 5;
+    if (difficulty === 'hard') {
+      time += 2;
+    } else if (difficulty === 'medium') {
+      time += 3;
+    } else {
+      time += 5;
+    }
 
     updateTime();
   }
+});
+
+// Settings btn Click
+settingsBtn.addEventListener('click', () => settings.classList.toggle('hide'));
+
+//Settings select
+settingsForm.addEventListener('change', (e) => {
+  difficulty = e.target.value;
+  localStorage.setItem('difficulty', difficulty);
 });
