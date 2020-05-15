4cce33e8df39dda42161ab3cd2b7ae129e365e79
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-05-15 10:34:31 -0500

    JS started. form listener async to API

---
+++ lyric_search/script.js
@@ -0,0 +1,37 @@
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const result = document.getElementById('result');
+const more = document.getElementById('more');
+
+const apiURL = 'https://api.lyrics.ovh';
+
+//Saerch by songs or artist
+async function searchSongs(term) {
+  //      fetch method
+  //      fetch(`${apiURL}/suggest/${term}`)
+  //     .then((res) => res.json())
+  //     .then((data) => console.log(data));
+
+  //  async await
+
+  const res = await fetch(`${apiURL}/suggest/${term}`);
+  const data = await res.json();
+
+  showData(data);
+}
+
+// Show songs and artist in DOM
+function showData(data) {}
+
+//Event listners
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value.trim();
+
+  if (!searchTerm) {
+    alert('Please enter a search term');
+  } else {
+    searchSongs(searchTerm);
+  }
+});
