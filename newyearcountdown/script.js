3bed6f08ce5245ca3dc39c862b2f8f697216ef27
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-06-03 16:52:18 -0500

    Added spinner for loading. Added next year to background. Complete

--- newyearcountdown/script.js
+++ newyearcountdown/script.js
@@ -3,10 +3,15 @@ const hours = document.getElementById('hours');
 const minutes = document.getElementById('minutes');
 const seconds = document.getElementById('seconds');
 const countdown = document.getElementById('countdown');
+const year = document.getElementById('year');
+const loading = document.getElementById('loading');
 
 const currentYear = new Date().getFullYear();
 
 const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+
+// Set background year
+year.innerText = currentYear + 1;
 
 function updateCountdown() {
   const currentTime = new Date();
@@ -24,5 +29,11 @@ function updateCountdown() {
   minutes.innerHTML = m < 10 ? '0' + m : m;
   seconds.innerHTML = s < 10 ? '0' + s : s;
 }
+
+// Show spinner before countdown
+setTimeout(() => {
+  loading.remove();
+  countdown.style.display = 'flex';
+}, 1000);
 
 setInterval(updateCountdown, 1000);
