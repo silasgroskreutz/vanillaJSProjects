adba56fb0fa96f4957e7efb2d58af0c8c6fb086b
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-06-07 10:39:22 -0500

    Scramble and sort added to JS

--- sortable-list/script.js
+++ sortable-list/script.js
@@ -23,11 +23,15 @@ createList();
 
 // Insert list items into DOM
 function createList() {
-  [...richestPeople].forEach((person, index) => {
-    const listItem = document.createElement('li');
+  [...richestPeople]
+    .map((a) => ({ value: a, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map((a) => a.value)
+    .forEach((person, index) => {
+      const listItem = document.createElement('li');
 
-    listItem.setAttribute('data-index', index);
-    listItem.innerHTML = `
+      listItem.setAttribute('data-index', index);
+      listItem.innerHTML = `
     <span class="number">${index + 1}</span>
     <div class="draggable" draggable="true">
         <p class="person-name">${person}</p>
@@ -35,8 +39,8 @@ function createList() {
     </div>
     `;
 
-    listItems.push(listItem);
+      listItems.push(listItem);
 
-    draggable_list.append(listItem);
-  });
+      draggable_list.append(listItem);
+    });
 }
