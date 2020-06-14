223f328a28378e7eb5c52de5897caf7e9130a53e
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-06-13 20:00:57 -0500

    Speech recognition complete. Added write and check functions to game.

--- speak-number-guess/script.js
+++ speak-number-guess/script.js
@@ -15,8 +15,47 @@ recognition.start();
 //Cpature user speech
 function onSpeak(e) {
   const msg = e.results[0][0].transcript;
-  // writeMessage(msg)
-  // checkMessage(msg)
+
+  writeMessage(msg);
+  checkNumber(msg);
+}
+
+//Write what user speaks
+function writeMessage(msg) {
+  msgEl.innerHTML = `
+    <div>You said: </div>
+    <span class="box">${msg}</span>
+  `;
+}
+
+// Check msg against number
+function checkNumber(msg) {
+  const num = +msg;
+
+  // Check if a valid number
+  if (Number.isNaN(num)) {
+    msgEl.innerHTML += '<div>That is not a valid number';
+    return;
+  }
+
+  //Check if it is in range
+  if (num > 100 || num < 1) {
+    msgEl.innerHTML += '<div>Number must be between 1 and 100</div>';
+    return;
+  }
+
+  //Check number
+  if (num === randomNum) {
+    document.body.innerHTML = `
+    <h2>Congrats! You have guessed the number! <br><br>
+    It was ${num}</h2>
+    <button class="play-again" id="play-again">Play Again</button>
+    `;
+  } else if (num > randomNum) {
+    msgEl.innerHTML += '<div>Go lower</div>';
+  } else {
+    msgEl.innerHTML += '<div>Go Higher</div>';
+  }
 }
 
 // Generate Random Number
@@ -26,3 +65,12 @@ function getRandomNumber() {
 
 // Speak result
 recognition.addEventListener('result', onSpeak);
+
+// End Speech Recognition Service
+recognition.addEventListener('end', () => recognition.start());
+
+document.body.addEventListener('click', (e) => {
+  if (e.target.id == 'play-again') {
+    window.location.reload();
+  }
+});
